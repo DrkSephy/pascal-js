25d7d7b4b43fe09a54d5a4f523d91395c2f6f139
David Leonard <sephirothcloud1025@yahoo.com>
2014-12-23 00:54:30 -0500

    Adding js folder

---
+++ js/scanner.js
@@ -0,0 +1,13 @@
+'use strict';   
+
+/**********************
+* Scanner constructor *
+**********************/
+
+function Scanner(){
+    this.tokens = []
+}
+
+Scanner.prototype.scan = function(){
+    console.log("Main scanner function")
+}

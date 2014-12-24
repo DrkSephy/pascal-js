6126c9195bc4d5a9d61ab849d10b85bb1ca053ed
David Leonard <sephirothcloud1025@yahoo.com>
2014-12-24 15:18:21 -0500

    main.js: Adding sample program string to scan

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 (function() {
     var scanner = new Scanner();
-    scanner.scan()
+    var program = "program assignment; var a, b, c: integer; BEGIN a := 8 * 2 ; b := a * 2 ; c := b / 7 ; END. "
+    scanner.scan(program)
     console.log(scanner.tokens);
 })();

3fcb3729f6c924e76dde3ff19dba06ee4376289d
David Leonard <sephirothcloud1025@yahoo.com>
2014-12-25 00:09:18 -0500

    Removing main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-'use strict'; 
-
-(function() {
-    var scanner = new Scanner();
-    var program = "program assignment; var a, b, c: integer; BEGIN a := 8 * 2 ; b := a * 2 ; c := b / 7 ; END. "
-    scanner.scan(program)
-    console.log(scanner.tokens);
-})();
